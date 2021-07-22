dc5d70defec3012a498653097da94888986110e8
Sam Terry <samterry@Sams-iMac.attlocal.net>
2021-07-21 23:32:20 -0500

    A more complete version of the early skeleton script. Adds a loop to accept input, training status updates, and more languages. This is of course impractically slow because the model must be retrained each time the script is run

--- app/identifier.py
+++ app/identifier.py
@@ -1,7 +1,10 @@
 import os
 
 
-langs = ['Dutch','German','Swedish','French']
+print("\n\nLanguage Identifier v0.0.1")
+print("Use 'exit()' to quit\n\n")
+
+langs = ['Dutch','German','Swedish','French','Spanish','Italian','Turkish']
 corpora = {}
 counts = {}
 model = {}
@@ -20,41 +23,60 @@ for lang in langs:
     model[lang] = {}
 
 
-for lang in langs:
 
+# Count all trigrams across the corpora and store counts for each language 
+progress = 0
+for lang in langs:
+    print("Training models...(%s/7)\n" % str(progress))
     for i in range( len(corpora[lang]) - 2):
         threeGram = corpora[lang][i:i+3]
         
+        # If the model for a language does not yet have a value for the trigram, initialize it to 0
         for l in langs:
             if threeGram not in counts[l]:
                 counts[l][threeGram] = 0
         
+        # Increment trigram count for the language in which we encountered it
         counts[lang][threeGram] += 1
     
+    # Calculate scores by dividing each trigram count by the total trigrams in each corpus
     for gram in counts[lang]:
         model[lang][gram] = counts[lang][gram] / float(len(corpora[lang]) - 2)
 
+    progress += 1
+print("Training models...(%s/7)\n\n" % str(progress))
+print("Done!\n\n")
+
+
+
+# Main loop for excepting queries
 utterance = raw_input("Please enter a string: ")
+while utterance != "exit()":
 
-uttGrams = []
-for j in range(len(utterance) - 2):
-    uttGrams.append(utterance[j:j+3])
+    # Break the string into trigrams
+    uttGrams = []
+    for j in range(len(utterance) - 2):
+        uttGrams.append(utterance[j:j+3])
 
 
-best_guess = langs[0]
-best_score = 0
-current_score = 0
+    best_guess = langs[0]
+    best_score = 0
+    current_score = 0
 
-for lang in langs:
-    for gram in uttGrams:
-        if gram in model[lang]:
-            current_score += model[lang][gram]
+    # For each language, if the trigram exists in the model
+    for lang in langs:
+        for gram in uttGrams:
+            if gram in model[lang]:
+                current_score += model[lang][gram] # Increase the score for the current language by the score for that trigram in said language
 
-    if current_score >= best_score:
-        best_score = current_score
-        best_guess = lang
-    # print(current_score)
-    current_score = 0        
-# print(current_score)
-# print("\n")
-print("The probability of this string occuring in %s is %s" % (best_guess, str(round(best_score, 2))))
+            # If at any point the language's score becomes the top score, it is now the best guess
+            if current_score >= best_score:
+                best_score = current_score
+                best_guess = lang
+
+        current_score = 0        
+        
+    print("This string is most likely in %s" % (best_guess))
+    print("\n")
+
+    utterance = raw_input("Please enter a string: ")
